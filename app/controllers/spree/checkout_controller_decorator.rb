c62c150652652d3125252929703a27d5001d97a3
Haukur Thorsteinsson <haukur@haukur.net>
2016-09-28 00:12:38 +0000

    Translation

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,14 +4,17 @@ module Spree
 
     private
 
-      def sufficient_loyalty_points
-        payment_method_ids = params[:order][:payments_attributes].collect do |payment|
-          payment["payment_method_id"]
-        end
-        if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)
-          flash[:error] = Spree.t(:insufficient_loyalty_points)
-          redirect_to spree.checkout_state_path(@order.state)
-        end
+    def sufficient_loyalty_points
+      if params[:use_existing_card] == 'yes'
+        payment_method_ids = [Spree::CreditCard.find_by(id: params[:order]['existing_card']).payment_method_id]
+      else
+        payment_method_ids = params[:order][:payments_attributes].collect { |index, payment| payment["payment_method_id"] }
       end
+
+      if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)
+        flash[:error] = Spree.t(:insufficient_loyalty_points)
+        redirect_to spree.checkout_state_path(@order.state)
+      end
+    end
   end
 end
