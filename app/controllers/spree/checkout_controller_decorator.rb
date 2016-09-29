695aca3ce8d50ce91680ca21ab41cf5262e8c7d3
haukurhrafn <haukurhrafn@gmail.com>
2016-09-29 22:46:55 +0000

    Handle using existing card

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -8,7 +8,9 @@ module Spree
       if params[:use_existing_card] == 'yes'
         payment_method_ids = [Spree::CreditCard.find_by(id: params[:order]['existing_card']).payment_method_id]
       else
-        payment_method_ids = params[:order][:payments_attributes].collect { |index, payment| payment["payment_method_id"] }
+        payment_method_ids = params[:order][:payments_attributes].collect do |payment|
+          payment["payment_method_id"]
+        end
       end
 
       if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)
