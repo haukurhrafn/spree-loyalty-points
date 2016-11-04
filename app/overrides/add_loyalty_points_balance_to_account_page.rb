35dde0c67eed78c465057490d06dcd6e847a6199
haukurhrafn <haukurhrafn@gmail.com>
2016-11-04 00:02:11 +0000

    fix

--- app/overrides/add_loyalty_points_balance_to_account_page.rb
+++ app/overrides/add_loyalty_points_balance_to_account_page.rb
@@ -4,6 +4,6 @@ Deface::Override.new(virtual_path: 'spree/users/show',
   text: "
     <dl id='loyalty-points-info'>
       <dt><i class='fa fa-star'></i><%= Spree.t(:loyalty_points_balance) %></dt>
-      <dd><%= @user.loyalty_points_balance %> <%= "(jafngildir " + equivalent_currency_balance.to_s + " kr.)" %> (<%= link_to Spree.t(:details), spree.loyalty_points_path %>)</dd>
+      <dd><%= @user.loyalty_points_balance %> (<%= link_to Spree.t(:details), spree.loyalty_points_path %>)</dd>
     </dl>
   ")
